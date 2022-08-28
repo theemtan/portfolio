9ae026b756a6753862d4c21308f6e67a5827341d
theemtan <52570151+theemtan@users.noreply.github.com>
2022-08-28 16:08:14 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+  ],
+  theme: {
+    fontFamily: {
+      'oswald': ['oswald', 'sans-serif'],
+      'lilita': ['"Lilita One"', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
